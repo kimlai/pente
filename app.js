2dd7722e9724064abd234e1c888d4171c7380f93
kimlai <hello@kimlaitrinh.me>
2020-06-24 20:23:32 +0200

    Add a slider to change DB in real time

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-document.getElementById("form").addEventListener("submit", e => {
-  e.preventDefault();
-  const data = new FormData(e.target);
-  const altitudeA = data.get("altitude-A");
-  const altitudeB = data.get("altitude-B");
-  const CB = data.get("distance-CB");
+const computeSlope = () => {
+  const data = new FormData(document.getElementById("form"));
+  const altitudeA = parseInt(data.get("altitude-A"), 10);
+  const altitudeB = parseInt(data.get("altitude-B"), 10);
+  const CB = parseInt(data.get("distance-CB"), 10);
   const AC = altitudeA - altitudeB;
 
   const slope = ((100 * AC) / CB).toFixed(2);
   const AB = Math.sqrt(AC * AC + CB * CB).toFixed(2);
   document.getElementById("slope").innerHTML = slope + "%";
   document.getElementById("distance-AB").innerHTML = AB + "m";
+};
+
+document.getElementById("slide-CB").addEventListener("input", e => {
+  document.getElementById("distance-CB").value = e.target.value;
+  computeSlope();
+});
+
+document.getElementById("form").addEventListener("submit", e => {
+  e.preventDefault();
+  computeSlope();
+
+  const CB = parseInt(document.getElementById("distance-CB").value, 10);
+  const slider = document.getElementById("slide-CB");
+  slider.classList.add("show");
+  slider.setAttribute("min", CB - 30);
+  slider.setAttribute("max", CB + 30);
+  slider.value = CB;
 });
