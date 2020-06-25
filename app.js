ed6b81c839fb1e51b7b51095e8105ee921717096
kimlai <hello@kimlaitrinh.me>
2020-06-25 13:16:39 +0200

    [stairs] add slider to manipulate L

--- app.js
+++ app.js
@@ -68,6 +68,14 @@ document.getElementById("slide-stairs-count").addEventListener("input", e => {
   computeStairs(stairsCount);
 });
 
+document.getElementById("slide-stairs-L").addEventListener("input", e => {
+  const L = e.target.value;
+  document.getElementById("stairs_L").value = L;
+  computeStairs(
+    parseInt(document.getElementById("slide-stairs-count").value, 10)
+  );
+});
+
 document.getElementById("stairs_form").addEventListener("submit", e => {
   e.preventDefault();
   const data = new FormData(e.target);
@@ -77,6 +85,8 @@ document.getElementById("stairs_form").addEventListener("submit", e => {
   const N = Math.round((2 * H + L) / 63);
   const minN = Math.round((2 * H + L) / 66);
   const maxN = Math.round((2 * H + L) / 60);
+  const minL = Math.round(N * 60 - 2 * H);
+  const maxL = Math.round(N * 66 - 2 * H);
   const h = H / N;
   const g = L / N;
   const formula = (2 * h + g).toFixed(2);
@@ -86,8 +96,15 @@ document.getElementById("stairs_form").addEventListener("submit", e => {
   document.getElementById("stairs_h").innerHTML = "h = " + h.toFixed(2) + "cm";
   document.getElementById("stairs_g").innerHTML = "g = " + g.toFixed(2) + "cm";
   document.getElementById("stairs_formula").innerHTML = "2h + g = " + formula;
-  const slider = document.getElementById("slide-stairs-count");
-  slider.setAttribute("min", minN);
-  slider.setAttribute("max", maxN);
-  slider.value = N;
+
+  const sliderN = document.getElementById("slide-stairs-count");
+  sliderN.setAttribute("min", minN);
+  sliderN.setAttribute("max", maxN);
+  sliderN.value = N;
+
+  const L_inMeters = toFloat(data.get("stairs_L"));
+  const sliderL = document.getElementById("slide-stairs-L");
+  sliderL.setAttribute("min", Math.max(0.01, minL / 100).toFixed(2));
+  sliderL.setAttribute("max", (maxL / 100).toFixed(2));
+  sliderL.value = toFloat(data.get("stairs_L"));
 });
